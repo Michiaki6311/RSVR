474c00fbb2590d116b93a5e0bc843bd5c3836619
0xBADDCAFE <0xBADDCAFE@users.noreply.github.com>
2016-11-09 04:05:09 +0900

    Organize routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,19 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  get 'signup', to: 'users#new'
   resources :users, except:[:show]
+  get 'signup', to: 'users#new'
+  get 'users/account', to: 'users#account', as: :account_user
+  get 'users/:accesstoken', to: 'users#show', as: :show_user
+
+  get 'sessions/new'
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
   delete 'logout', to: 'sessions#destroy'
-  get '/users/account', to: 'users#account'
-  get '/users/:accesstoken', to: 'users#show', as: 'show_user'
 
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   root to: 'static_pages#home'
   get '/help', to: 'static_pages#help'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
