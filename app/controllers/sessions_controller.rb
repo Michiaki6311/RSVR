bedd612f57abf4bdf2dcd9b5f04bf6db05c23a06
0xBADDCAFE <0xBADDCAFE@users.noreply.github.com>
2016-11-09 02:16:02 +0900

    Use flash#now to disapper unwanted persistent flash content

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,12 @@ class SessionsController < ApplicationController
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
       session[:user_id] = @user.id
-      flash[:info] = "logged in as #{@user.name}"
+      flash.now[:info] = "logged in as #{@user.name}"
 
 
       redirect_to "/users/#{@user.access_token}"
     else
-      flash[:danger] = 'invalid email/password combination'
+      flash.now[:danger] = 'invalid email/password combination'
       render 'new'
     end
   end
