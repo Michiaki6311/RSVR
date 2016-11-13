63f7715d86e68a126b3a44b6429aa1881cce8e21
0xBADDCAFE <0xBADDCAFE@users.noreply.github.com>
2016-11-13 23:36:20 +0900

    Add full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  # Return fully each page title
+  def full_title(page_title = '')
+    base_title = "RSVR"
+    if page_title.empty?
+      base_title
+    else
+      "#{page_title} | #{base_title}"
+    end
+  end
 end
