6b7944f5d4a44cef85d4c279c0c83f5341f771c9
0xBADDCAFE <0xBADDCAFE@users.noreply.github.com>
2016-11-13 19:47:54 +0900

    schema.rb is updated

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161030041549) do
+ActiveRecord::Schema.define(version: 20161111031726) do
 
   create_table "active_admin_comments", force: :cascade do |t|
     t.string   "namespace"
@@ -41,6 +41,14 @@ ActiveRecord::Schema.define(version: 20161030041549) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admin_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true
+  end
+
+  create_table "user_interim_registrations", force: :cascade do |t|
+    t.string   "email"
+    t.string   "access_token", limit: 6, null: false
+    t.datetime "created_at",             null: false
+    t.datetime "updated_at",             null: false
+    t.index ["access_token"], name: "index_user_interim_registrations_on_access_token", unique: true
   end
 
   create_table "users", force: :cascade do |t|
