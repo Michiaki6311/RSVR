e510cbd396191da3834fa8aef9e0c6feac26765a
Michiaki6311 <kanadebito63@yahoo.co.jp>
2016-11-16 21:33:59 +0900

    add mail setting and gem config'

--- Gemfile
+++ Gemfile
@@ -11,6 +11,7 @@ gem 'activeadmin', github: 'activeadmin'
 gem 'inherited_resources', github: 'activeadmin/inherited_resources'
 gem 'devise'
 gem 'html2slim'
+gem 'config'
 # Use SCSS for stylesheets
 gem 'sass-rails', '~> 5.0'
 # Use Uglifier as compressor for JavaScript assets
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -30,28 +30,37 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => 587,
+    :user_name => Setting.gmail.user_name,
+    :password => Setting.gmail.password,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
-  config.action_mailer.perform_caching = false
+    config.action_mailer.perform_caching = false
 
-  # Print deprecation notices to the Rails logger.
-  config.active_support.deprecation = :log
+    # Print deprecation notices to the Rails logger.
+    config.active_support.deprecation = :log
 
-  # Raise an error on page load if there are pending migrations.
-  config.active_record.migration_error = :page_load
+    # Raise an error on page load if there are pending migrations.
+    config.active_record.migration_error = :page_load
 
-  # Debug mode disables concatenation and preprocessing of assets.
-  # This option may cause significant delays in view rendering with a large
-  # number of complex assets.
-  config.assets.debug = true
+    # Debug mode disables concatenation and preprocessing of assets.
+    # This option may cause significant delays in view rendering with a large
+    # number of complex assets.
+    config.assets.debug = true
 
-  # Suppress logger output for asset requests.
-  config.assets.quiet = true
+    # Suppress logger output for asset requests.
+    config.assets.quiet = true
 
-  # Raises error for missing translations
-  # config.action_view.raise_on_missing_translations = true
+    # Raises error for missing translations
+    # config.action_view.raise_on_missing_translations = true
 
-  # Use an evented file watcher to asynchronously detect changes in source code,
-  # routes, locales, etc. This feature depends on the listen gem.
-  # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+    # Use an evented file watcher to asynchronously detect changes in source code,
+    # routes, locales, etc. This feature depends on the listen gem.
+    # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 end
