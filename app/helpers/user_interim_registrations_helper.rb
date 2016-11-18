849995229d91b90f272db466256d40fd90717c1e
Michiaki6311 <kanadebito63@yahoo.co.jp>
2016-11-18 22:11:48 +0900

    remove unuse files and method

--- app/helpers/user_interim_registrations_helper.rb
+++
@@ -1,2 +0,0 @@
-module UserInterimRegistrationsHelper
-end
--- test/controllers/user_interim_registrations_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class UserInterimRegistrationsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
