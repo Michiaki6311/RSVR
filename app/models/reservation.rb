a313726cdc41844cbb4d3216d049fd498b6aaf95
Michiaki6311 <kanadebito63@yahoo.co.jp>
2016-11-29 16:52:54 +0900

    fix datetime validation

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -8,6 +8,9 @@ class Reservation < ApplicationRecord
   validates :end_time,
     presence: true
 
+  validates :facility_id,
+    presence: true
+
   validate :date_cannot_be_in_the_past,
     :date_validate,
     :date_uniqueness,
@@ -26,11 +29,11 @@ class Reservation < ApplicationRecord
 
   def date_uniqueness
     errors.add(:end_time,"その時間帯にはすでに予約があります。") if
-    Reservation.where(strat_time: (self.strat_time)...(self.end_time)).present? || Reservation.where(end_time: ((self.strat_time)+Rational(1, 24 * 60 * 60))..(self.end_time)).present?
+    Reservation.where("strat_time >= ? AND strat_time < ? AND facility_id = ?",self.strat_time,self.end_time,self.facility_id).present? || Reservation.where("end_time > ? AND end_time <= ? AND facility_id = ?",self.strat_time,self.end_time,self.facility_id).present?
   end
 
   def date_uniqueness_two
     errors.add(:end_time,"その時間帯にはすでに予約があります。") if
-    Reservation.where('strat_time <= ? AND end_time >= ?',self.strat_time,self.end_time).present?
+    Reservation.where('strat_time <= ? AND end_time >= ? AND facility_id = ?',self.strat_time,self.end_time,self.facility_id).present?
   end
 end
