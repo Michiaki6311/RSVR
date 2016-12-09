d9ac9261b3a4a177bb1d22f10997dc9f05d94b22
Michiaki6311 <kanadebito63@yahoo.co.jp>
2016-12-09 10:12:18 +0900

    add reservations history

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def account
     @user = User.find(current_user.id)
+    @my_reservations = Reservation.where(user_id: current_user.id) if user_signed_in?
   end
 
 end
